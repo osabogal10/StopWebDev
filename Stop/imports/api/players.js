270be87b79cb376cfc20ad663e44ba1166c200d0
Daniel Cagua Ennis <d.cagua10@uniandes.edu.co>
2018-10-02 18:02:20 -0500

    Players update

--- Stop/imports/api/players.js
+++ Stop/imports/api/players.js
@@ -1,3 +1,36 @@
+import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
 
 export const Players = new Mongo.Collection('players');
+
+if (Meteor.isServer) {
+	Meteor.publish ('players', () => {
+		return Players.find({});
+	});
+}
+
+Meteor.methods(
+  {
+    'players.add': function() 
+    {
+      const name = Meteor.user().username;
+      const win = Meteor.user().win;
+      const lost = Meteor.user().lost;
+
+      if(!name)
+      {
+        throw new Meteor.Error('Not Authorized');
+      }
+      Players.upsert(
+        {name},
+        {
+          name,
+          win:0,
+          lost:0
+        });
+
+      const player = Players.findOne({name});
+      return player;
+    }
+  }
+);

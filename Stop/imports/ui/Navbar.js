3be9bf31662bd548fa2ffa6ebde64aee1b81c938
Daniel Cagua Ennis <d.cagua10@uniandes.edu.co>
2018-10-07 23:19:43 -0500

    Class->ClassName
    Pinche consola alv

--- Stop/imports/ui/Navbar.js
+++ Stop/imports/ui/Navbar.js
@@ -12,35 +12,35 @@ export class Navbar extends Component
         return (
             <div className="navbar">
             {Meteor.user() ?
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
-                <ul class="navbar-nav">
-                <li class="nav-item active">
-                <a class="nav-link" href="/">Welcome to the STOP Online Game - Web Development Uniandes<span class="sr-only">(current)</span></a>
+                <div className="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+                <ul className="navbar-nav">
+                <li className="nav-item active">
+                <a className="nav-link" href="/">Welcome to the STOP Online Game - Web Development Uniandes<span className="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link">Logged In! Welcome:  </a>
+                <li className="nav-item">
+                <a className="nav-link">Logged In! Welcome:  </a>
                 </li>
                 </ul>
                 <AccountsUIWrapper/>
                 </div>
                 </nav>
                 :
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
                 
-                <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
-                <ul class="navbar-nav">
-                <li class="nav-item active">
-                <a class="nav-link">Welcome to the STOP Online Game - Web Development Uniandes<span class="sr-only">(current)</span></a>
+                <div className="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+                <ul className="navbar-nav">
+                <li className="nav-item active">
+                <a className="nav-link">Welcome to the STOP Online Game - Web Development Uniandes<span className="sr-only">(current)</span></a>
                 </li>
-                <li class="nav-item">
-                <a class="nav-link">Need to log to start playing</a>
+                <li className="nav-item">
+                <a className="nav-link">Need to log to start playing</a>
                 </li>
                 </ul>
                 </div>
@@ -63,27 +63,27 @@ export class Navbar extends Component
         };
     })(Navbar);
     
-    {/* <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+    {/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
     </button>
     
-    <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
-    <ul class="navbar-nav">
-    <li class="nav-item">
-    <a class="nav-link" href="/">Orlando Sabogal Rojas</a>
+    <div className="collapse navbar-collapse justify-content-md-center" id="navbarsExample08">
+    <ul className="navbar-nav">
+    <li className="nav-item">
+    <a className="nav-link" href="/">Orlando Sabogal Rojas</a>
     </li>
-    <li class="nav-item">
-    <a class="nav-link" href="/"></a>
+    <li className="nav-item">
+    <a className="nav-link" href="/"></a>
     </li>
-    <li class="nav-item active">
-    <a class="nav-link" href="/">Welcome to the STOP Online Game - Web Development Uniandes<span class="sr-only">(current)</span></a>
+    <li className="nav-item active">
+    <a className="nav-link" href="/">Welcome to the STOP Online Game - Web Development Uniandes<span className="sr-only">(current)</span></a>
     </li>
-    <li class="nav-item">
-    <a class="nav-link" href="/"></a>
+    <li className="nav-item">
+    <a className="nav-link" href="/"></a>
     </li>
-    <li class="nav-item">
-    <a class="nav-link" href="/">Daniel Cagua Ennis</a>
+    <li className="nav-item">
+    <a className="nav-link" href="/">Daniel Cagua Ennis</a>
     </li>
     </ul>
     </div>
